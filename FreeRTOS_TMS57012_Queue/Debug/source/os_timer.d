c24ac701ebae0a9df11292e60119b666e34da758
lipavashka <lipavashka@gmail.com>
2017-11-17 14:56:37 +0200

    Signed-off-by: lipavashka <lipavashka@gmail.com>

---
+++ FreeRTOS_TMS57012_Queue/Debug/source/os_timer.d
@@ -0,0 +1,35 @@
+# FIXED
+
+source/os_timer.obj: ../source/os_timer.c
+source/os_timer.obj: E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdlib.h
+source/os_timer.obj: E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/linkage.h
+source/os_timer.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/FreeRTOS.h
+source/os_timer.obj: E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stddef.h
+source/os_timer.obj: E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h
+source/os_timer.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/FreeRTOSConfig.h
+source/os_timer.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/os_projdefs.h
+source/os_timer.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/os_portable.h
+source/os_timer.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/os_portmacro.h
+source/os_timer.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/os_mpu_wrappers.h
+source/os_timer.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/os_task.h
+source/os_timer.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/os_list.h
+source/os_timer.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/os_queue.h
+source/os_timer.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/os_timer.h
+source/os_timer.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/os_list.h
+
+../source/os_timer.c: 
+E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdlib.h: 
+E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/linkage.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/FreeRTOS.h: 
+E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stddef.h: 
+E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/FreeRTOSConfig.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/os_projdefs.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/os_portable.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/os_portmacro.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/os_mpu_wrappers.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/os_task.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/os_list.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/os_queue.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/os_timer.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Queue/include/os_list.h: 
