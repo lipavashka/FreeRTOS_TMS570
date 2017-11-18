f9c8f67ef447648a2df1560d90fabe7da5b22ae7
lipavashka <lipavashka@gmail.com>
2017-11-18 17:01:23 +0200

    Signed-off-by: lipavashka <lipavashka@gmail.com>

---
+++ FreeRTOS_TMS57012_BinSemaphore/Debug/source/adc.d
@@ -0,0 +1,39 @@
+# FIXED
+
+source/adc.obj: ../source/adc.c
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/adc.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/reg_adc.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/sys_common.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/hal_stdtypes.h
+source/adc.obj: E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h
+source/adc.obj: E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/sys_vim.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/reg_vim.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/FreeRTOS.h
+source/adc.obj: E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stddef.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/FreeRTOSConfig.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/os_projdefs.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/os_portable.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/os_portmacro.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/os_mpu_wrappers.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/os_semphr.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/os_queue.h
+
+../source/adc.c: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/adc.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/reg_adc.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/sys_common.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/hal_stdtypes.h: 
+E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h: 
+E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/sys_vim.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/reg_vim.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/FreeRTOS.h: 
+E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stddef.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/FreeRTOSConfig.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/os_projdefs.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/os_portable.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/os_portmacro.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/os_mpu_wrappers.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/os_semphr.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_BinSemaphore/include/os_queue.h: 
