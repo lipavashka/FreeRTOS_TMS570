00ef492c72d1fa0d3ffb5b92aad890d88c420e08
lipavashka <lipavashka@gmail.com>
2017-11-20 12:24:40 +0200

    Signed-off-by: lipavashka <lipavashka@gmail.com>

---
+++ FreeRTOS_TMS57012_CountingSemaphore/Debug/source/adc.d
@@ -0,0 +1,39 @@
+# FIXED
+
+source/adc.obj: ../source/adc.c
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/adc.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_adc.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/sys_common.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/hal_stdtypes.h
+source/adc.obj: E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h
+source/adc.obj: E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/sys_vim.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_vim.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/FreeRTOS.h
+source/adc.obj: E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stddef.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/FreeRTOSConfig.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/os_projdefs.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/os_portable.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/os_portmacro.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/os_mpu_wrappers.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/os_semphr.h
+source/adc.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/os_queue.h
+
+../source/adc.c: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/adc.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_adc.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/sys_common.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/hal_stdtypes.h: 
+E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h: 
+E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/sys_vim.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_vim.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/FreeRTOS.h: 
+E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stddef.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/FreeRTOSConfig.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/os_projdefs.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/os_portable.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/os_portmacro.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/os_mpu_wrappers.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/os_semphr.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/os_queue.h: 
