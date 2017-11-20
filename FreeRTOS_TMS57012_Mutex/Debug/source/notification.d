afdeba28802a2e261038fd5b91d729c36cae3075
lipavashka <lipavashka@gmail.com>
2017-11-20 12:23:18 +0200

    Signed-off-by: lipavashka <lipavashka@gmail.com>

---
+++ FreeRTOS_TMS57012_Mutex/Debug/source/notification.d
@@ -0,0 +1,107 @@
+# FIXED
+
+source/notification.obj: ../source/notification.c
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/esm.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_esm.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/sys_common.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/hal_stdtypes.h
+source/notification.obj: E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h
+source/notification.obj: E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/sys_selftest.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_pbist.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_stc.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_efc.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/sys_core.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/system.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_system.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_gio.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_flash.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_tcram.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/sys_vim.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_vim.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/adc.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_adc.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/can.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_can.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/mibspi.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_mibspi.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/het.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_het.h
+source/notification.obj: E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/string.h
+source/notification.obj: E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/linkage.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/htu.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_htu.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/gio.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/lin.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_lin.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/sci.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_sci.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/spi.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_spi.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/dcc.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_dcc.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/i2c.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_i2c.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/crc.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_crc.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/etpwm.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_etpwm.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/eqep.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_eqep.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/ecap.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_ecap.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/sys_dma.h
+source/notification.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_dma.h
+
+../source/notification.c: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/esm.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_esm.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/sys_common.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/hal_stdtypes.h: 
+E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h: 
+E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/sys_selftest.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_pbist.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_stc.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_efc.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/sys_core.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/system.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_system.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_gio.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_flash.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_tcram.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/sys_vim.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_vim.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/adc.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_adc.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/can.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_can.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/mibspi.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_mibspi.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/het.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_het.h: 
+E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/string.h: 
+E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/linkage.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/htu.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_htu.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/gio.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/lin.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_lin.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/sci.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_sci.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/spi.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_spi.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/dcc.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_dcc.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/i2c.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_i2c.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/crc.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_crc.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/etpwm.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_etpwm.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/eqep.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_eqep.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/ecap.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_ecap.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/sys_dma.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_CountingSemaphore/include/reg_dma.h: 
