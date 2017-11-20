c9c739906f965448527640be1ba1ee23afbb77b6
lipavashka <lipavashka@gmail.com>
2017-11-20 20:57:54 +0200

    Signed-off-by: lipavashka <lipavashka@gmail.com>

---
+++ FreeRTOS_TMS57012_Mutex/Debug/source/os_mpu_wrappers.d
@@ -0,0 +1,33 @@
+# FIXED
+
+source/os_mpu_wrappers.obj: ../source/os_mpu_wrappers.c
+source/os_mpu_wrappers.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/FreeRTOS.h
+source/os_mpu_wrappers.obj: E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stddef.h
+source/os_mpu_wrappers.obj: E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h
+source/os_mpu_wrappers.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/FreeRTOSConfig.h
+source/os_mpu_wrappers.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_projdefs.h
+source/os_mpu_wrappers.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_portable.h
+source/os_mpu_wrappers.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_portmacro.h
+source/os_mpu_wrappers.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_mpu_wrappers.h
+source/os_mpu_wrappers.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_task.h
+source/os_mpu_wrappers.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_list.h
+source/os_mpu_wrappers.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_queue.h
+source/os_mpu_wrappers.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_timer.h
+source/os_mpu_wrappers.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_list.h
+source/os_mpu_wrappers.obj: L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_event_groups.h
+
+../source/os_mpu_wrappers.c: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/FreeRTOS.h: 
+E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stddef.h: 
+E:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/FreeRTOSConfig.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_projdefs.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_portable.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_portmacro.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_mpu_wrappers.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_task.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_list.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_queue.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_timer.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_list.h: 
+L:/Git/FreeRTOS_TMS570/FreeRTOS_TMS57012_Mutex/include/os_event_groups.h: 
